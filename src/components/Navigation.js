33d769ea8ed9afe037d8f7952820cc2f068ccd6e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-12 14:41:35 -0600

    finished step 6

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
-import React, {useContext } from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { CartContext } from '../contexts/CartContext'
 
 const Navigation = () => {
-	const { cart } = useContext(CartContext)
+	const cart = useContext(CartContext)
 
 	return (
 		<div className="navigation">
